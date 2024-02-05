832c9c642928f29f0ac5ff56efe7decd001cd6ea
Adam Becker <adambecker3@gmail.com>
2024-02-05 17:28:51 -0500

    initialise backend app

--- apps/share-price-backend/jest.config.ts
+++ apps/share-price-backend/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'share-price-platform',
+  displayName: 'share-price-backend',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/share-price-platform',
+  coverageDirectory: '../../coverage/apps/share-price-backend',
 };
